f387bcd2065678ff0201d81bf4f074473cedad0d
Madison Johnson <m.johnson@colostate.edu>
2025-11-19 10:36:04 -0700

    Comments from class- MJ

--- Flint_Code.R
+++ Flint_Code.R
@@ -2,12 +2,17 @@
 # Option 1: tidytuesdayR R package 
 ## install.packages("tidytuesdayR")
 
+
 library(tidyverse)
+library(tidytuesdayR)
 
 tuesdata <- tidytuesdayR::tt_load('2025-11-04')
 
 flint_mdeq <- tuesdata$flint_mdeq
 flint_vt <- tuesdata$flint_vt
+#MJ 11/19: Andreas and I are confused beyond this point
+#MJ 11/19: KP, do we need that test12187 data set below?
+#MJ 11/19: Or just the two datasets above that I loaded? 
 
 
 # Set the data directory. Change this if your data is in a different location.
