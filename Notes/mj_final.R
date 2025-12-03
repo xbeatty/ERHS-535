9fa5298580487fe22fcd22009cb3d5b93592ce36
Madison Johnson <m.johnson@colostate.edu>
2025-12-03 15:22:40 -0700

    Worked on graphing overweight vs stunded children

--- Notes/mj_final.R
+++ Notes/mj_final.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(tidytuesdayR)
+library(ggplot2)
+library(ggthemes)
+library(forcats)
 #use tigris color
 #compare malnurished vs overweight kids line plots in 12 countries
 #levels 14, 15
@@ -14,8 +17,50 @@ levels(factor_foodsec)
 factor_foodsec <- as.factor(food_security$Area)
 levels(factor_foodsec)
 
+children_food <- food_south %>% 
+  filter(Item %in% c(
+    "Number of children under 5 years of age who are overweight (modeled estimates) (million)", 
+    "Number of children under 5 years of age who are stunted (modeled estimates) (million)")) %>% 
+  select(Year_Start, Year_End, Area, Item, Value)
 
+children_food %>% 
+  ggplot(aes(x = Area, y = Value, Fill = Item)) +
+  geom_col()
 
-children_df <- food_security %>%
-  filter(str_detect(Item, regex("children", 
-                                ignore_case = TRUE)))
+children_food %>% 
+  ggplot(aes(x = fct_reorder(Area, Value), y = Value, fill = Item)) +
+  geom_col(position = "dodge") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.direction = "vertical",
+        legend.position = "bottom") +
+  scale_fill_manual(name = "Child Condition",
+    values = c(
+      "Number of children under 5 years of age who are overweight (modeled estimates) (million)" = "blue",
+      "Number of children under 5 years of age who are stunted (modeled estimates) (million)" = "limegreen"),
+    labels = c(
+      "Number of children under 5 years of age who are overweight (modeled estimates) (million)" = "Overweight",
+      "Number of children under 5 years of age who are stunted (modeled estimates) (million)" = "Stunted")) +
+  labs(x = NULL,
+       title = "Number of Children Modeled to be Overweight or Stunted",
+       y = "Number of Children (millions)")
+#possibility of adding facet_wrap by Item too 
+
+#individual graphs made- I dont think needed if I can do facet_wrap above
+overweight <- children_food %>%
+  filter(Item == "Number of children under 5 years of age who are overweight (modeled estimates) (million)") %>% 
+  select(Year_Start, Year_End, Area, Item, Value)
+stunted <- children_food %>% 
+  filter(Item == "Number of children under 5 years of age who are stunted (modeled estimates) (million)") %>% 
+  select(Year_Start, Year_End, Area, Item, Value)
+
+overweight %>% 
+  ggplot(aes(x = fct_reorder(Area, Value), y = Value, fill = Area)) +
+  geom_col() +
+  theme(legend.position = "none") +
+  labs(title = "Number of children under 5 overweight")
+
+stunted %>% 
+  ggplot(aes(x = fct_reorder(Area, Value), y = Value, fill = Area)) +
+  geom_col() +
+  theme(legend.position = "none") +
+  labs(title = "Number of children under 5 overweight")
