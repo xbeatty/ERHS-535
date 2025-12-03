11e704d887969cb07efd321914af4821254b02f9
Madison Johnson <m.johnson@colostate.edu>
2025-12-03 11:14:28 -0700

    class notes from 12/3

--- Notes/mj_final.R
+++ Notes/mj_final.R
@@ -1,5 +1,10 @@
 library(tidyverse)
 library(tidytuesdayR)
+#use tigris color
+#compare malnurished vs overweight kids line plots in 12 countries
+#levels 14, 15
+#xaxis as time and y-axis is measurement in % (mal and over)
+
 
 food_security <- readr::read_csv(
   'https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-10-14/food_security.csv')
@@ -8,3 +13,9 @@ factor_foodsec <- as.factor(food_security$Item)
 levels(factor_foodsec)
 factor_foodsec <- as.factor(food_security$Area)
 levels(factor_foodsec)
+
+
+
+children_df <- food_security %>%
+  filter(str_detect(Item, regex("children", 
+                                ignore_case = TRUE)))
