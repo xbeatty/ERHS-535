e209ce95cf2f4629a371545f6e44d76b77919d82
Xuan Beatty <xuan.beatty@colostate.edu>
2025-12-11 18:15:09 -0700

    Updates to facet graphs

--- Notes/Xuan.R
+++ Notes/Xuan.R
@@ -12,22 +12,23 @@ p_ts_facets <- ggplot(
     group = Area,
     text = paste0(
       "Area: ", Area,
-      "<br>Year_End: ", Year_End,
+      "<br>Year: ", Year_End,
       "<br>Value: ", round(Value, 2), " ", Unit
     )
   )
 ) +
-  geom_line(color = "steelblue") +
-  geom_point(color = "steelblue") +
+  geom_line(color = "darkblue") +
+  geom_point(color = "darkblue") +
   facet_wrap(~ Area) +
   labs(
-    x = "Year_End",
+    x = "Year",        # Changed axis label
     y = "Percent",
     title = "Trend in food insecurity by country"
   ) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.title.x = element_text(margin = margin(t = 10))  # Move x label down
   )
 
 ggplotly(p_ts_facets, tooltip = "text")
-
